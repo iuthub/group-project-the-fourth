20fa8458a23768f15b1755827cb783b6330043fb
Elina Abdrashitova <1910025.elina.abdrashitova@gmail.com>
2021-05-18 23:47:55 +0500

    laravel login page(master,header,footer,login)(email,password) button Submit(doesnt do anything)

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-print "Hello";
-print "Hello everybody";
-print "Hi!";
-?>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-dark bg-dark" style="background-color: #A3A4B6;">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">Technimals</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="/myorders">Facts</a></li>
+            </ul>
+            <form action="/search" class="navbar-form navbar-left">
+                <div class="form-group">
+                    <input type="text" name="query" class="form-control search-box" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Search</button>
+            </form>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
