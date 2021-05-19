bcbda0f8ca50918b35e967ea0957dfa7dbbd7921
Elina Abdrashitova <1910025.elina.abdrashitova@gmail.com>
2021-05-19 23:15:12 +0500

    footer updated

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <div style="clear:both" class="panel panel-default">
     <div class="panel-body">
-        Panel content
+        © 2021 Technimals Inc. All rights reserved.
     </div>
-    <div class="panel-footer">Panel footer</div>
+    <div class="panel-footer">Contact us: abdrashitova1elina@gmail.com</div>
 </div>
