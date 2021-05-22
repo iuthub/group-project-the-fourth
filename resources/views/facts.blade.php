1882e91191a98cdd4cfabc696b036ab55baf5514
Dilnozakhon Mirsaidova <77745030+di-mirs@users.noreply.github.com>
2021-05-23 02:43:25 +0500

    mission page style

--- resources/views/facts.blade.php
+++ resources/views/facts.blade.php
@@ -4,11 +4,11 @@
         <div class="container">
             <h1 class="display-4">Facts</h1>
             <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-        </div>
-    </div>
 <ul>
    <li> <a href="project1">See project 1</a> </li>
    <li> <a href="project2">See project 2</a> </li>
     <li> <a href="project3">See project 3</a> </li>
 </ul>
+    </div>
+    </div>
 @endsection
