9a69439b69803cd8e74fe047a58ab5b7b6163c98
Dilnozakhon Mirsaidova <77745030+di-mirs@users.noreply.github.com>
2021-05-23 03:50:49 +0500

    home page style

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,8 +3,8 @@
     <div class="content-page" >
         <div class="jumbotron jumbotron-fluid"  style="background-color: #A3A4B6">
             <div class="container">
-                <h1 class="display-4" >Technimals!</h1>
-                <p class="lead"  > <h3 style="margin-left:20px;"> <strong> Animal abuser is a looser. </strong> </h3>  </p>
+                <h1 class="display-4" id="post_page" >Technimals!</h1>
+                <p class="lead" > <h3 style="margin-left:20px;" > <strong> Animal abuser is a looser. </strong> </h3>  </p>
                 <div >
                     <img class="home-img" src="{{ asset('/homepic1.png') }}" alt="Who we are" title="">
                     <img class="home-img" src="{{ asset('/homepic2.png') }}" alt="Who we are" title="">
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -2,14 +2,14 @@
 @section("content")
     <div class="jumbotron jumbotron-fluid" style="background-color: #A3A4B6 ;">
         <div class="container" id="mission">
-            <h1 class="display-4" style="padding-bottom: 10px;">Mission</h1>
-            <p class="lead"> We, <strong>Technimals group</strong>, are here to support innovative ideas to save world of animals and
+            <h1 class="display-4" style="padding-bottom: 10px;margin-left:20px;">Mission</h1>
+            <p class="lead" style="margin-left:20px;" > We, <strong>Technimals group</strong>, are here to support innovative ideas to save world of animals and
                     contribute to the development of biotechnology and bioengineering.</p>
             <?php
             $image2_url='https://caroltorgan.com/wp-content/uploads/2016/03/Picture5.jpg';
             ?>
             <img src="<?php echo $image2_url;?> " >
-            <p class="lead">Our <strong> main goal </strong> is to introduce new projects to the world and  attract sponsors to support
+            <p class="lead" style="margin-left:20px;">Our <strong> main goal </strong> is to introduce new projects to the world and  attract sponsors to support
                 them.</p>
             <?php
             $image_url='https://design-academia.net/wp-content/uploads/2020/10/fig03-1024x460.jpg';
