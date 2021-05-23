f2609c7442ba91b712f2dac81f035000d98b6318
U1910076 <78978314+U1910076@users.noreply.github.com>
2021-05-23 09:56:10 +0500

    Update facts.blade.php

--- resources/views/facts.blade.php
+++ resources/views/facts.blade.php
@@ -4,6 +4,16 @@
         <div class="container">
             <h1 class="display-4">Facts</h1>
             <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+ <div>
+                <img src="{{ asset('/fact1.png') }}" alt="fact1" title="" style="width: 460px; height: 600px ">
+                <img src="{{ asset('/fact2.png') }}" alt="fact2" title="" style="width: 460px; height: 600px ">
+                <p></p>
+                <img src="{{ asset('/fact3.png') }}" alt="fact3" title="" style="width: 460px; height: 600px ">
+                <img src="{{ asset('/fact4.png') }}" alt="fact4" title="" style="width: 460px; height: 600px ">
+                <p></p>
+                <img src="{{ asset('/fact5.png') }}" alt="fact5" title="" style="width: 460px; height: 600px ">
+                <img src="{{ asset('/fact6.png') }}" alt="fact6" title="" style="width: 460px; height: 600px ">
+            </div>
 <ul>
    <li> <a href="project1">See project 1</a> </li>
    <li> <a href="project2">See project 2</a> </li>
