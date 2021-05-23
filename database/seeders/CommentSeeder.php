3e30c3ffc8ba0ce3c8fc4ca79c6512983fcea835
Elina Abdrashitova <1910025.elina.abdrashitova@gmail.com>
2021-05-23 05:58:06 +0500

    comments added

---
+++ database/seeders/CommentSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CommentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('comments')->insert([
+            'user_id'=>'1',
+            'article_id'=>'1',
+            'comment'=>'Great:)'
+        ]);
+    }
+}
