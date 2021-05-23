b6ba862aa9e903f4251094a9e406da3fe7ef1a1b
Dilnozakhon Mirsaidova <77745030+di-mirs@users.noreply.github.com>
2021-05-23 13:51:46 +0500

    facts page

--- resources/views/facts.blade.php
+++ resources/views/facts.blade.php
@@ -3,8 +3,12 @@
     <div class="jumbotron jumbotron-fluid" style="background-color: #A3A4B6 ;">
         <div class="container">
             <h1 class="display-4" style="text-shadow: 3px 3px #A3A4B6 ;" >Facts</h1>
-            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
- <div>
+            <ul >
+                <li class="proj"  > <a class="ref" href="project1">See project 1</a> </li>
+                <li class="proj"  > <a class="ref" href="project2">See project 2</a> </li>
+                <li class="proj" > <a  class="ref" href="project3">See project 3</a> </li>
+            </ul>
+            <div>
                 <img src="{{ asset('/fact1.png') }}" alt="fact1" title="" style="width: 460px; height: 600px ">
                 <img src="{{ asset('/fact2.png') }}" alt="fact2" title="" style="width: 460px; height: 600px ">
                 <p></p>
@@ -14,11 +18,6 @@
                 <img src="{{ asset('/fact5.png') }}" alt="fact5" title="" style="width: 460px; height: 600px ">
                 <img src="{{ asset('/fact6.png') }}" alt="fact6" title="" style="width: 460px; height: 600px ">
             </div>
-<ul>
-   <li> <a href="project1">See project 1</a> </li>
-   <li> <a href="project2">See project 2</a> </li>
-    <li> <a href="project3">See project 3</a> </li>
-</ul>
-    </div>
+        </div>
     </div>
 @endsection
